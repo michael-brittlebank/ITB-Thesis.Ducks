0b6a169f3ecc0984658176cf55d9d2746ea119d1
mike-stumpf <mike.a.stumpf@gmail.com>
2017-06-19 08:39:23 +0100

    workouts - added save workout logic

--- bootstrapper.js
+++ bootstrapper.js
@@ -2,9 +2,11 @@
 import userReducer from './ducks/user';
 import adminReducer from './ducks/admin';
 import configReducer from './ducks/config';
+import workoutReducer from './ducks/workout';
 
 export default {
     configState: configReducer,
     userState: userReducer,
-    adminState: adminReducer
+    adminState: adminReducer,
+    workoutState: workoutReducer
 };
---
+++ ducks/workout.js
@@ -0,0 +1,139 @@
+import axios from 'axios';
+
+import requestService from '../services/request';
+
+export const types = {
+    GET_EXERCISES_REQUEST: 'WORKOUT/GET_EXERCISES_REQUEST',
+    GET_EXERCISES_SUCCESS: 'WORKOUT/GET_EXERCISES_SUCCESS',
+    GET_EXERCISES_FAILURE: 'WORKOUT/GET_EXERCISES_FAILURE',
+    GET_WORKOUTS_REQUEST: 'WORKOUT/GET_WORKOUTS_REQUEST',
+    GET_WORKOUTS_SUCCESS: 'WORKOUT/GET_WORKOUTS_SUCCESS',
+    GET_WORKOUTS_FAILURE: 'WORKOUT/GET_WORKOUTS_FAILURE',
+    SAVE_WORKOUTS_REQUEST: 'WORKOUT/SAVE_WORKOUTS_REQUEST',
+    SAVE_WORKOUTS_SUCCESS: 'WORKOUT/SAVE_WORKOUTS_SUCCESS',
+    SAVE_WORKOUTS_FAILURE: 'WORKOUT/SAVE_WORKOUTS_FAILURE'
+};
+
+let defaultExercises = [],
+    defaultWorkouts = [],
+    defaultResponse = {};
+
+export const initialState = {
+    workouts: defaultWorkouts,
+    exercises: defaultExercises,
+    isLoading: false,
+    response: defaultResponse
+};
+
+//reducers
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case types.SAVE_WORKOUTS_REQUEST:
+        case types.GET_WORKOUTS_REQUEST:
+        case types.GET_EXERCISES_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+                response: defaultResponse
+            };
+        case types.GET_WORKOUTS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                workouts: action.workouts,
+                response: action.response
+            };
+        case types.GET_EXERCISES_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                exercises: action.exercises,
+                response: action.response
+            };
+        case types.SAVE_WORKOUTS_SUCCESS:
+        case types.SAVE_WORKOUTS_FAILURE:
+        case types.GET_WORKOUTS_FAILURE:
+        case types.GET_EXERCISES_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                response: action.response
+            };
+        default:
+            return state
+    }
+}
+
+export const actions = {
+    getExercises: function(store){
+        return function (dispatch) {
+            dispatch({type:types.GET_EXERCISES_REQUEST});
+            return axios({
+                method: 'GET',
+                url: requestService.getApiUrl(store)+'/workouts/exercises',
+                headers: requestService.getSessionHeaders(store)
+            })
+                .then((response) => {
+                    dispatch({
+                        type: types.GET_EXERCISES_SUCCESS,
+                        exercises: response.data,
+                        response: response
+                    });
+                })
+                .catch((error) => {
+                    dispatch({
+                        type: types.GET_EXERCISES_FAILURE,
+                        response: error.response
+                    });
+                });
+        };
+    },
+    getWorkouts: function(store){
+        return function (dispatch) {
+            dispatch({type:types.GET_WORKOUTS_REQUEST});
+            return axios({
+                method: 'GET',
+                url: requestService.getApiUrl(store)+'/workouts',
+                headers: requestService.getSessionHeaders(store)
+            })
+                .then((response) => {
+                    dispatch({
+                        type: types.GET_WORKOUTS_SUCCESS,
+                        workouts: response.data,
+                        response: response
+                    });
+                })
+                .catch((error) => {
+                    dispatch({
+                        type: types.GET_WORKOUTS_FAILURE,
+                        response: error.response
+                    });
+                });
+        };
+    },
+    saveWorkout: function(store, exercises){
+        return function (dispatch) {
+            dispatch({type:types.SAVE_WORKOUTS_REQUEST});
+            return axios({
+                method: 'PUT',
+                url: requestService.getApiUrl(store)+'/workouts',
+                headers: requestService.getSessionHeaders(store),
+                data: {
+                    exercises: exercises
+                }
+            })
+                .then((response) => {
+                    dispatch({
+                        type: types.SAVE_WORKOUTS_SUCCESS,
+                        response: response
+                    });
+                })
+                .catch((error) => {
+                    dispatch({
+                        type: types.SAVE_WORKOUTS_FAILURE,
+                        response: error.response
+                    });
+                });
+        };
+    },
+};
